4bba5ac782f62deb1c7e3f7ae96c9f19cb60ca5c
ruiju <ruijun789@gmail.com>
2024-04-18 11:19:00 +0800

    Revert "StatsCard & PlantDetails Fixes"
    This reverts commit 10574f807bbfd5e563eb642b68e023c6beeb4428.

--- src/components/StatsCard/StatsCard.jsx
+++ src/components/StatsCard/StatsCard.jsx
@@ -4,23 +4,6 @@ import "./StatsCard.css"
 import Lottie from "lottie-react"
 
 const StatsCard = (props) => {
-  const formatScientificNotation = (val, dp) => {
-    const exponent = Math.floor(Math.log10(Math.abs(val)));
-    const mantissa = val / Math.pow(10, exponent);
-    if (exponent === 0) {
-      return [`${mantissa.toFixed(dp)}`,  ''];
-    }
-    return [`${mantissa.toFixed(dp)}`, `${exponent}`];
-  };
-
-  // Format the value
-  const value = props.value
-  console.log("statsCard", value)
-  var formattedValue = [value.toFixed(2), ''];
-  if (props.value > 100 || props.value < 0.1) {
-    formattedValue = formatScientificNotation(props.value, 2);
-  }
-  
   return (
     <div className='StatsCard'>
       <div className='LeftSide'>
@@ -32,12 +15,7 @@ const StatsCard = (props) => {
         </div>
       </div>
       <div className='Value'>
-        {(formattedValue[1] === '') ? (<span>{formattedValue[0]}</span>) :
-          (<span>{formattedValue[0]}
-            <span style={{fontSize:'25px'}}>×10</span>
-            <span style={{fontSize:'15px', verticalAlign: 'top'}}> {formattedValue[1]}</span>
-          </span>)}
-        
+        <span>{props.value}</span>
         <span>{props.unit}</span>
       </div>
       
